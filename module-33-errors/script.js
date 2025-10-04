309eaf7f5248046f3732cf8e1985c496c6348dde
RifaAndhika <rifaandhika9@gmail.com>
2025-10-04 22:33:56 +0700

    errors

---
+++ module-33-errors/script.js
@@ -0,0 +1,11 @@
+//syntax errors
+
+// console.log('hello world';
+
+// //type errors
+// let num = null;
+// console.log(num.toString());
+
+//reference errors
+// let arr = [1,2,3];
+// console.log(arr1);
